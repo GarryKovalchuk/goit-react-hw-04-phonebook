254b05af89c7c5713a234e2d984bb58fa6751dec
Garry <vorzel47051@gmail.com>
2024-01-29 22:19:11 +0200

    react04phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,21 +5,13 @@ import { nanoid } from 'nanoid';
 import { useState, useEffect } from 'react';
 
 export const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
+  const initialContacts = () => {
     const contacts = localStorage.getItem('contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      setContacts(parsedContacts);
-    }
-  }, []);
+    return contacts ? JSON.parse(contacts) : [];
+  };
+
+  const [contacts, setContacts] = useState(initialContacts);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     console.log('renewed contacts');
